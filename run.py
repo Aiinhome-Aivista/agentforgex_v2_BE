022742ced11ab402653e5586459bcfae9be58668
Your Name <you@example.com>
2026-05-18 12:01:15 +0530

    port change

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ from flask_cors import CORS
 app = create_app()
 CORS(app)
 if __name__ == "__main__":
-    port = int(os.getenv("FLASK_PORT", 3004))
+    port = int(os.getenv("FLASK_PORT", 3006))
     debug = os.getenv("FLASK_ENV", "production").lower() == "development"
     app.run(host="0.0.0.0", port=port, debug=debug)
