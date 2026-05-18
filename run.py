0f660e1204a999a01eec1b4a28177fc03994c9b0
Your Name <you@example.com>
2026-05-18 12:39:55 +0530

    cors remove

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import os
 from app import create_app
-from flask_cors import CORS
+
 
 app = create_app()
-CORS(app)
+
 if __name__ == "__main__":
     port = int(os.getenv("FLASK_PORT", 3007))
     debug = os.getenv("FLASK_ENV", "production").lower() == "development"
