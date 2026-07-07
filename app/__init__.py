6643f2493966ca83d18b97842abfac2c62738872
HP Developer <5ynt4x.3rr0r.ss@gmail.com>
2026-07-07 13:49:39 +0530

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -36,7 +36,7 @@ def create_app() -> Flask:
         origins=origins,
         supports_credentials=True,
         allow_headers=["Content-Type", "Authorization"],
-        methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     )
 
     from app.api.routes import api_bp
